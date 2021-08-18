a0905d1c458e09556f054bd32056fbfe329b3e76
itsEit <28101524+itsEit@users.noreply.github.com>
2021-08-17 22:00:30 -0700

    Works, but state is not updated before render

--- src/components/gear/GearSet.js
+++ src/components/gear/GearSet.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Button } from "primereact/button";
@@ -6,9 +6,31 @@ import GearSearch from "./GearSearch";
 import "./GearSet.css";
 
 function GearSet(props) {
-  // const [gearList, setGearList] = useState(props.gearList);
+  // const [isLoading, setIsLoading] = useState(false);
+  const [tblOptions, setTblOptions] = useState([
+    { field: "_id", header: "ID", visible: false, stat: false },
+    { field: "name", header: "Name", visible: true, stat: false },
+    { field: "slotName", header: "Slot", visible: true, stat: false },
+  ]);
 
-  // let headerTitle = 'Gear Sets'
+  useEffect(() => {
+    // setIsLoading(true)
+    const items = props.gearList;
+    let options = tblOptions;
+
+    items.forEach((item) => {
+      if (item.stats) {
+        const itemKeys = Object.keys(item.stats);
+        itemKeys.forEach((key) => {
+          const fieldMatch = options.find((option) => option.field === key);
+          if (!fieldMatch) {
+            options.push({ field: key, header: key, visible: false, stat: true });
+          }
+        });
+      }
+    });
+    setTblOptions(options);
+  }, [props.gearList, tblOptions]);
 
   let colVisible = {
     isIdVisible: false,
@@ -24,6 +46,26 @@ function GearSet(props) {
     isCHRVisible: true,
   };
 
+  const toggleFilter = (field) => {
+    // console.log(field);
+  };
+
+  const dynamicFilters = tblOptions.map((item) => {
+    return (
+      item.stat && (
+        <Button
+          key={item.field}
+          type="button"
+          label={item.field}
+          className="p-button-sm p-button-info p-button-outlined p-mr-1"
+          onClick={toggleFilter(item.field)}
+        />
+      )
+    );
+  });
+
+  console.log(dynamicFilters)
+
   const headerBar = (
     <div className="table-header">
       <div>
@@ -31,61 +73,55 @@ function GearSet(props) {
           <div className="p-m-0">
             <GearSearch onSearchText={props.onSearchText} />
           </div>
-          <div className="p-d-flex">
-            <div className="p-m-1">
-              <Button type="button" label="Save" className="p-d-block p-mx-auto p-button-info" />
-            </div>
-            <div className="p-m-1">
-              <Button type="button" label="Delete" className="p-d-block p-mx-auto p-button-danger" />
-            </div>
+          <div className="p-pt-2">
+            <span>{dynamicFilters}</span>
           </div>
         </div>
       </div>
     </div>
   );
 
-  // const footer = (
-  //   <div className="p-d-flex p-jc-end">
-  //     <div style={{ width: "30%" }}>hello</div>
-  //     <div style={{ width: "8.5%" }}>hello</div>
-  //     <div style={{ width: "8.5%", textAlign: "center", border: "1px solid #e9ecef" }}>STR</div>
-  //     <div style={{ width: "8.5%", textAlign: "center" }}>DEX</div>
-  //     <div style={{ width: "8.5%", textAlign: "center" }}>VIT</div>
-  //     <div style={{ width: "8.5%", textAlign: "center" }}>AGI</div>
-  //     <div style={{ width: "8.5%", textAlign: "center" }}>INT</div>
-  //     <div style={{ width: "8.5%", textAlign: "center" }}>MND</div>
-  //     <div style={{ width: "8.5%", textAlign: "center" }}>CHR</div>
-  //   </div>
-  // );
+  const statTotals = (props) => {
+    const items = props.gearList;
+    let stats = {};
+    items.forEach((item) => {
+      if (item.stats.STR) {
+        stats.STR = (stats.STR || 0) + item.stats.STR;
+      }
+    });
+    return stats;
+  };
 
   const footer = (
     <table className="test p-datatable-thead">
-      <tr role="row">
-        <th style={{ width: "38.5%", textAlign: "right", background:"#f8f9fa"}}>
-          <span class="p-column-title p-mr-5">Set Totals</span>
-        </th>
-        <th style={{ width: "8.5%", textAlign: "center" }}>
-          <span class="p-column-title">STR</span>
-        </th>
-        <th style={{ width: "8.5%", textAlign: "center" }}>
-          <span class="p-column-title">DEX</span>
-        </th>
-        <th style={{ width: "8.5%", textAlign: "center" }}>
-          <span class="p-column-title">VIT</span>
-        </th>
-        <th style={{ width: "8.5%", textAlign: "center" }}>
-          <span class="p-column-title">AGI</span>
-        </th>
-        <th style={{ width: "8.5%", textAlign: "center" }}>
-          <span class="p-column-title">INT</span>
-        </th>
-        <th style={{ width: "8.5%", textAlign: "center" }}>
-          <span class="p-column-title">MND</span>
-        </th>
-        <th style={{ width: "8.5%", textAlign: "center" }}>
-          <span class="p-column-title">CHR</span>
-        </th>
-      </tr>
+      <tfoot>
+        <tr role="row">
+          <th style={{ width: "38.5%", textAlign: "right", background: "#f8f9fa" }}>
+            <span className="p-column-title p-mr-5">Set Totals</span>
+          </th>
+          <th style={{ width: "8.5%", textAlign: "center" }}>
+            <span className="p-column-title">{statTotals(props).STR}</span>
+          </th>
+          <th style={{ width: "8.5%", textAlign: "center" }}>
+            <span className="p-column-title">DEX</span>
+          </th>
+          <th style={{ width: "8.5%", textAlign: "center" }}>
+            <span className="p-column-title">VIT</span>
+          </th>
+          <th style={{ width: "8.5%", textAlign: "center" }}>
+            <span className="p-column-title">AGI</span>
+          </th>
+          <th style={{ width: "8.5%", textAlign: "center" }}>
+            <span className="p-column-title">INT</span>
+          </th>
+          <th style={{ width: "8.5%", textAlign: "center" }}>
+            <span className="p-column-title">MND</span>
+          </th>
+          <th style={{ width: "8.5%", textAlign: "center" }}>
+            <span className="p-column-title">CHR</span>
+          </th>
+        </tr>
+      </tfoot>
     </table>
   );
   return (
@@ -103,8 +139,8 @@ function GearSet(props) {
         {colVisible.isNameVisible && <Column field="name" header="Name" style={{ width: "30%" }}></Column>}
         {colVisible.isLevelVisible && <Column field="level" header="Level"></Column>}
         {colVisible.isSlotsVisible && <Column field="slotName" header="Slot" style={{ width: "8.5%" }}></Column>}
-        {colVisible.isSTRVisible && <Column field="stats.STR" header="STR" style={{ width: "8.5%", textAlign: "center" }}></Column>}
-        {colVisible.isDEXVisible && <Column field="stats.DEX" header="DEX" style={{ width: "8.5%", textAlign: "center" }}></Column>}
+        {colVisible.isSTRVisible && <Column field="stats.STR" header="GearHaste" style={{ width: "8.5%", textAlign: "center" }}></Column>}
+        {colVisible.isDEXVisible && <Column field="stats.DEX" header="GearHaste" style={{ width: "8.5%", textAlign: "center" }}></Column>}
         {colVisible.isVITVisible && <Column field="stats.VIT" header="VIT" style={{ width: "8.5%", textAlign: "center" }}></Column>}
         {colVisible.isAGIVisible && <Column field="stats.AGI" header="AGI" style={{ width: "8.5%", textAlign: "center" }}></Column>}
         {colVisible.isINTVisible && <Column field="stats.INT" header="INT" style={{ width: "8.5%", textAlign: "center" }}></Column>}
